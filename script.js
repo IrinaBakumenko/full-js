4ffb969084d79ee731b626939400238fe10b3c88
Ira <ibakumenko997@gmail.com>
2021-01-16 21:16:57 +0300

    Code refactoring: functions are implemented as separate methods of an object

--- script.js
+++ script.js
@@ -1,80 +1,83 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('How many films have you watched?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('How many films have you watched?', '');
-    }
-}
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-}
-
-function rememberMyFilms() {
-    for(let i = 0; i < 2; i++) {
-        const a = prompt('One of the last movies you watched?', ''),
-            b = prompt('How much would you rate it?', '');
-
-        if (a != null && b != null && a!= '' && b != '' && a.length < 50){
-            console.log('done');
-            personalMovieDB.movies[a] = b;
-        } else {
-            console.log('error');
-            i--;
+    privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt('How many films have you watched?', '');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('How many films have you watched?', '');
         }
+    },
+    rememberMyFilms: function () {
+        for(let i = 0; i < 2; i++) {
+            const a = prompt('One of the last movies you watched?', ''),
+                b = prompt('How much would you rate it?', '');
+    
+            if (a != null && b != null && a!= '' && b != '' && a.length < 50){
+                console.log('done');
+                personalMovieDB.movies[a] = b;
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count <10) {
+            console.log('Quite a few movies watched');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('You are a classic spectator');
+        } else if (personalMovieDB.count >= 30) {
+            console.log('You are a movie fan');
+        } else {
+            console.log('Error');
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for(let i = 1; i <= 3; i++) {
+            let genre = prompt(`Your favorite genre is numbered ${i}?`);
+
+            if (genre === '' || genre == null) {
+                console.log('Error');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+        }
+        
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Favorite genre ${i + 1} is ${item}`);
+        });
     }
-}
+};
 
-rememberMyFilms();
+/* 4. Задание на урок:
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count <10) {
-        console.log('Quite a few movies watched');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('You are a classic spectator');
-    } else if (personalMovieDB.count >= 30) {
-        console.log('You are a movie fan');
-    } else {
-        console.log('Error');
-    }
-}
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
 
-detectPersonalLevel();
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
 
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for(let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Your favorite genre is numbered ${i}?`);
-    }
-}
-
-writeYourGenres();
-
-/* 3. Задание на урок:
-
-1) Первую часть задания повторить по уроку
-
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-*/
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
