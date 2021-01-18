9a5c7a30d4d072d217c31cf405a6ddcbe2853770
Ira <ibakumenko997@gmail.com>
2021-01-18 21:52:34 +0300

    Code refactoring

--- js/script.js
+++ js/script.js
@@ -1,23 +1,7 @@
 'use strict';
 
-const box = document.getElementById('box');
-
-console.log(box);
-
-const btns = document.getElementsByTagName('button');
-
-console.log(btns[1]);
-
-const circles = document.getElementsByClassName('circle');
-
-console.log(circles);
-
-const hearts = document.querySelectorAll('.heart');
-
-hearts.forEach(item => {
-    console.log(item);
-});
-
-const oneHeart = document.querySelector('.heart');
-
-console.log(oneHeart);
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('heart'),
+      oneHeart = document.querySelector('.heart');
