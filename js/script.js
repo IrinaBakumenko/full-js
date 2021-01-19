ef929caac8e65be86965d43dd759a166b24dc244
Ira <ibakumenko997@gmail.com>
2021-01-19 22:15:18 +0300

    Removing and adding items to a page using JS

--- js/script.js
+++ js/script.js
@@ -1,59 +1,50 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
 'use strict';
 
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName('circle'),
-      wrapper = document.querySelector('.wrapper'),
-      hearts = wrapper.querySelectorAll('.heart'),
-      oneHeart = wrapper.querySelector('.heart');
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
 
-// box.style.backgroundColor = 'pink';
-// box.style.width = '500px';
-const num = 500;
-box.style.cssText = `background-color: blue; width: ${num}px`;
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
 
-btns[1].style.borderRadius = '100%';
-circles[0].style.backgroundColor = 'red';
-
-// for (let i = 0; i < hearts.length; i++) {
-//     hearts[i].style.backgroundColor = 'pink';
-// }
-
-hearts.forEach(item => {
-    item.style.backgroundColor = 'pink';
+adv.forEach (item => {
+    item.remove();
 });
 
-const div = document.createElement('div');
-// const text = document.createTextNode('some text');
+genre.textContent = 'драма';
 
-div.classList.add('black');
+poster.style.backgroundImage = 'url("img/bg.jpg")';
 
-// document.body.append(div);
-// document.querySelector('.wrapper').append(div);
+movieList.innerHTML = "";
 
-wrapper.append(div);
-// wrapper.appendChild(div);
+movieDB.movies.sort();
 
-
-// wrapper.prepend(div);
-
-// hearts[0].before(div);
-// hearts[0].after(div);
-
-// wrapper.insertBefore(div, hearts[1]);
-
-
-// circles[0].remove();
-// wrapper.removeChild(hearts[1]);
-
-hearts[0].replaceWith(circles[0]);
-// wrapper.replaceChild(circles[0], hearts[0]);
-
-div.innerHTML = "<h1>Hello world</h1>";
-
-// div.textContent = "Hello";
-
-// div.insertAdjacentHTML('beforebegin', '<h2>Hello</h2>');
-// div.insertAdjacentHTML('afterbegin', '<h2>Hello</h2>');
-// div.insertAdjacentHTML('beforeend', '<h2>Hello</h2>');
-// div.insertAdjacentHTML('afterend', '<h2>Hello</h2>');
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML +=  `
+        <li class="promo__interactive-item">${i +1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
