c9220c00ece67a71017416be54202e4d775e11df
Ira <ibakumenko997@gmail.com>
2021-01-29 04:28:07 +0300

    ClassLict and event delegation

--- js/script.js
+++ js/script.js
@@ -1,108 +1,47 @@
-/* Задания на урок:
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
 
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(0);
+// console.log(btns[1].classList.add('red'));
+// console.log(btns[1].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
 
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// }
 
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')) {
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
 
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
-"Добавляем любимый фильм"
-
-5) Фильмы должны быть отсортированы по алфавиту */
-
-'use strict';
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    const movieDB = {
-        movies: [
-            "Логан",
-            "Лига справедливости",
-            "Ла-ла лэнд",
-            "Одержимость",
-            "Скотт Пилигрим против..."
-        ]
-    };
-    
-    const adv = document.querySelectorAll('.promo__adv img'),
-          poster = document.querySelector('.promo__bg'),
-          genre = poster.querySelector('.promo__genre'),
-          movieList = document.querySelector('.promo__interactive-list'),
-          addForm = document.querySelector('form.add'),
-          addInput = addForm.querySelector('.adding__input'),
-          checkbox = addForm.querySelector('[type="checkbox"]');
-    
-    addForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-
-        let newFilm = addInput.value;
-        const favorite = checkbox.checked;
-
-        if (newFilm) {
-            if(newFilm.length > 21) {
-                newFilm = `${newFilm.substring(0, 22)}...`;
-            }
-
-            if(favorite) {
-                console.log("Добавляем любимый фильм");
-            }
-
-            movieDB.movies.push(newFilm);
-            sortArr(movieDB.movies);
-    
-            createMovieList(movieDB.movies, movieList);
-    
-        }
-
-        event.target.reset();
-
-    });
-    
-    const deleteAdv = (arr) => {
-        arr.forEach (item => {
-            item.remove();
-        });
-    };
-
-    const makeChanges = () => {
-        genre.textContent = 'драма';
-    
-        poster.style.backgroundImage = 'url("img/bg.jpg")';
-    };
-
-    const sortArr = (arr) => {
-        arr.sort();
-    };
-
-    function createMovieList(films, parent) {
-        parent.innerHTML = "";
-        sortArr(films);
-        
-        films.forEach((film, i) => {
-            parent.innerHTML +=  `
-                <li class="promo__interactive-item">${i +1} ${film}
-                    <div class="delete"></div>
-                </li>
-            `;
-        }); 
-
-        document.querySelectorAll('.delete').forEach((btn, i) => {
-            btn.addEventListener('click', () => {
-                btn.parentElement.remove();
-                movieDB.movies.splice(i, 1);
-
-                createMovieList(films, parent);
-            });
-        });
-    }
-
-    deleteAdv(adv);
-    makeChanges();
-    createMovieList(movieDB.movies, movieList);
-
+    btns[1].classList.toggle('red'); 
 });
+
+// console.log(btns[0].className);
+
+wrapper.addEventListener('click', (event) => {
+    // console.dir(event.target);
+    // if (event.target && event.target.tagName == "BUTTON") {
+    //     console.log('ff');
+    // }
+    // if (event.target && event.target.classList.contains('red')) {
+    //     console.log('ff');
+    // }
+    if (event.target && event.target.matches('button.red')) {
+         console.log('ff');
+    }
+});
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('Hello');
+//     });
+// });
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
